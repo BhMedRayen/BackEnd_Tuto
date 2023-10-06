eb0ff500e7f28a099b8b3e9f71167501e3262e74
BhMedRayen <rayenmohamed2310@gmail.com>
2023-10-06 23:26:29 +0100

    user signUp

--- models/user.js
+++ models/user.js
@@ -9,6 +9,12 @@ const User = mongoose.model('User',{
     },
     age : {
         type : Number
+    },
+    email : {
+        type : String
+    },
+    password : {
+        type  : String
     }
 })
 
